2b241ef1505f84d8f351ee8e832191b2c55d2d60
Puppycoder88 <jaingarima1795@gmail.com>
2024-06-27 14:42:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import CreateNote from "./CreateNote";
+import Note from "./Note";
+import Footer from "./Footer";
+
+const App = () => {
+  const [addItem, setAddItem] = useState([]);
+
+  const addNote = (note) => {
+    // alert("I am clicked")
+    setAddItem((prevData) => {
+      return [...prevData, note];
+    });
+    console.log(note);
+  };
+
+  const onDelete = (id) => {
+    setAddItem((olddata) =>
+      olddata.filter((currdata, indx) => {
+        return indx !== id;
+      })
+    );
+  };
+  return (
+    <>
+      <Header />
+      <CreateNote passNote={addNote} />
+
+      {addItem.map((val, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={val.title}
+            content={val.content}
+            DeleteItem={onDelete}
+          />
+        );
+      })}
+      <Footer />
+    </>
+  );
+};
+export default App;
